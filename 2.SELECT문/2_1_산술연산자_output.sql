1a2f3e26059fcebadfee379a3db580b5a097b04c
DaaEun <didekdms123@gmail.com>
2021-08-27 14:01:13 +0900

    산술연산자

---
+++ 2.SELECT문/2_1_산술연산자_output.sql
@@ -0,0 +1,29 @@
+
+LAST_NAME                     SALARY SALARY+300
+------------------------- ---------- ----------
+Whalen                          4400       4700
+Hartstein                      13000      13300
+Fay                             6000       6300
+Higgins                        12008      12308
+Gietz                           8300       8600
+King                           24000      24300
+Kochhar                        17000      17300
+De Haan                        17000      17300
+Hunold                          9000       9300
+Ernst                           6000       6300
+Lorentz                         4200       4500
+
+LAST_NAME                     SALARY SALARY+300
+------------------------- ---------- ----------
+Mourgos                         5800       6100
+Rajs                            3500       3800
+Davies                          3100       3400
+Matos                           2600       2900
+Vargas                          2500       2800
+Zlotkey                        10500      10800
+Abel                           11000      11300
+Taylor                          8600       8900
+Grant                           7000       7300
+
+20개 행이 선택되었습니다. 
+
