9c13843f07b9c58c66b5dbe450fbaaa60379d501
DaaEun <didekdms123@gmail.com>
2021-08-30 23:29:33 +0900

    3.데이터제한 및 정렬 실습 완료

--- SQL/3.데이터제한및정렬_실습/practice15.sql
+++ SQL/3.데이터제한및정렬_실습/practice15.sql
@@ -2,7 +2,7 @@
     15. 커미션이 20%인 모든 사원의 성, 급여 및 커미션을 표시하도록 lab_03_06.sql을 수정합니다. 
     lab_03_06.sql을 lab_03_15.sql로 다시 저장합니다.
 */
-SELECT last_name Employee, salary "Monthly Salary", commission_pct
+SELECT last_name "Employee", salary "Monthly Salary", commission_pct
 FROM employees
 WHERE commission_pct = 0.2;
 
